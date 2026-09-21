127e5e593fd9139b036c012d6b8ca03c32086c9c
controlesmineras <controlesminerask@gmail.com>
2026-09-20 21:49:05 -0500

    Conectar backend empresarial de Apps Script

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 // Pegue aquí la URL de la implementación de Google Apps Script.
 window.CONTROL_EXPLOSIVOS_CONFIG = {
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwcKfD2YajE6TvOmV-bw1pOUFtZioHcGk-2RTHH0YOOQUIrukMMIjS8qMct2QzrRB6obQ/exec",
 };
